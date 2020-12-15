fa5a0475f94f5b0bf2b61ada2af8300bb8755fe2
Drew Harper <druharper@gmail.com>
2020-12-15 15:02:28 -0600

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <nav>
+      <p><router-link :to="{ name: 'Home' }">Home</router-link></p>
+      <p><router-link :to="{ name: 'About' }">About</router-link></p>
+      <p>User Profile</p>
+    </nav>
+    <div>
+      <span v-for="(id, index) in userIds" :key="index">
+        <router-link :to="{ name: 'ViewProfile', params: {user_id: id}}"
+        >Profile {{ id }}</router-link>
+      </span>
+      <button @click="goBack">Go Back</button>
+      <button @click="goHome">Go Home</button>
+      <button @click="goForward">Go Forward</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Navbar',
+    data() {
+      return {
+        userIds: ['1', '2', '3', '4']
+      }
+    },
+    methods: {
+      goHome(){
+        this.$router.push({ name: 'Home' })
+      },
+      goBack(){
+        this.$router.go(-1)
+      },
+      goForward(){
+        this.$router.go(1)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  nav {
+    /* background: lightgrey; */
+    padding: 1rem;
+    display: flex;
+    justify-content: space-around;
+  }
+
+  nav a {
+
+  }
+
+  div span a {
+    margin: 0 .5rem;
+  }
+
+  a.router-link-exact-active {
+    color: purple;
+    background: yellow;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import About from '@/components/About'
+import ViewProfile from '@/components/ViewProfile'
+
+Vue.use(Router)
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '*',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/profile/:user_id',
+      name: 'ViewProfile',
+      component: ViewProfile
+    }
+  ]
+})
+
+const originalPush = Router.prototype.push;
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => {
+    if (err.name !== 'NavigationDuplicated') throw err
+  });
+}
